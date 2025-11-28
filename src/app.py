c22c8e2e1c242a7e273ca6fa2b0f96d79a687c5c
Kiliangil88 <149784768+Kiliangil88@users.noreply.github.com>
2025-11-28 20:24:23 +0000

    Proyecto en proceso

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ st.title("Diabetes - Model prediciton")
         
 # Obtain values from form
 val1 = st.slider('Glucose', min_value = 50.0, max_value = 200.0, step = 1.0)
-val2 = st.slider('BloodPressure', min_value = 40.0, max_value = 120, step = 1.0)
+val2 = st.slider('BloodPressure', min_value = 40.0, max_value = 120.0, step = 1.0)
 val3 = st.slider('SkinThickness', min_value = 10.0, max_value = 60.0, step = 1.0)
 val4 = st.slider('Insulin', min_value = 2.0, max_value = 250.0, step = 1.0)
 val5 = st.slider('BMI', min_value = 15.0, max_value = 50.0, step = 1.0)
