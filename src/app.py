63210fa5284c8d8155d86e25bfa70dd5343f4fba
Kiliangil88 <149784768+Kiliangil88@users.noreply.github.com>
2025-11-28 19:37:58 +0000

    Proyecto en curso

--- src/app.py
+++ src/app.py
@@ -1,4 +1,25 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+from pickle import load
+import joblib
 
-# your code here
+model = joblib.load("mejor_modelo_tree.pkl")
+class_dict = {
+    "0": "No Diabético",
+    "1": "Diabético"
+}
+st.title("Diabetes - Model prediciton")    
+        
+        # Obtain values from form
+val1 = st.slider('Glucose', min_value = 50.0, max_value = 200.0, step = 1)
+val2 = st.slider('BloodPressure', min_value = 40.0, max_value = 120, step = 0.1)
+val3 = st.slider('SkinThickness', min_value = 10.0, max_value = 60.0, step = 0.1)
+val4 = st.slider('Insulin', min_value = 2.0, max_value = 250.0, step = 0.1)
+val5 = st.slider('BMI', min_value = 15.0, max_value = 50.0, step = 0.1)
+val6 = st.slider('Age', min_value = 18.0, max_value = 90.0, step = 0.1)
+
+if st.button("Predict"):
+    data = [[val1, val2, val3, val4, val5, val6]]
+    prediction = str(model.predict(data)[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
+
